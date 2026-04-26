961f086f97598eca7813f9f5c601f23e7d810720
aarushi862 <aarushipandey1810@gmail.com>
2026-04-27 00:59:07 +0530

    Update API URL for Render production deployment

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000',
+  baseURL: 'https://chatterbox-backend-4v4p.onrender.com',
 });
 
 api.interceptors.request.use((config) => {
